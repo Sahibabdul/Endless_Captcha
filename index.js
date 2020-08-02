323f372b58275e7c85f13c3d6e884328138da85e
Sahibabdul <trump69trump@gmail.com>
2020-08-02 15:02:51 +0200

    added basic structure

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "https://unpkg.com/react@16/umd/react.development.js"
+import ReactDOM from "https://unpkg.com/react-dom@16/umd/react-dom.development.js"
+
+ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"))
